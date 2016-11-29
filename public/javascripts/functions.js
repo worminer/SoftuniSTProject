66a838531ad43cdbf24f070f5bb487619d1bff3f
worminer <worminer@gmail.com>
2016-11-29 18:55:19 +0200

    + fixed design   + fixed: top menu colours   + fixed: pictures now take the size of the container and dont make the container overflow   + fixed: side menu now is bootstrap comlient and it load the actual categories(and shows how much moves are in the category)   + other: design related changes

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,8 +1,8 @@
 $(function() {
-	$('.sidebar .widget ul li:first-child').addClass('first');
-	$('#navigation ul li:first-child').addClass('first');
-	$('#navigation ul li:last-child').addClass('last');
-	$('.footer-nav ul li:first-child').addClass('first');
+    $('.sidebar .widget ul li:first-child').addClass('first');
+    $('#navigation ul li:first-child').addClass('first');
+    $('#navigation ul li:last-child').addClass('last');
+    $('.footer-nav ul li:first-child').addClass('first');
 
 	$('a.popup').colorbox({
 		onComplete: function(){
